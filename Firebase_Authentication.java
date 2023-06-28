610c2046c7b2361ec20e1c72905165ee24b95ac1
AJNatsu <107909543+AJNatsu@users.noreply.github.com>
2023-06-29 00:19:36 +0900

    Create Firebase_Authentication.java

---
+++ Firebase_Authentication.java
@@ -0,0 +1,61 @@
+private EditText mEmail, mPassword;
+   private Button mLogin, mRegistration;
+   private FirebaseAuth mAuth;
+   private FirebaseAuth.AuthStateListener firebaseAuthListener;
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+   super.onCreate(savedInstanceState);
+   setContentView(R.layout.activity_driver_login);
+
+   mAuth = FirebaseAuth.getInstance();
+
+   firebaseAuthListener= new FirebaseAuth.AuthStateListener() {
+     @Override
+     public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+   FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+   if(user!=null){
+     Intent intent = new Intent(DriverLoginActivity.this, DriverMapActivity.class);
+     startActivity(intent);
+     finish();
+     return;
+
+mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(DriverLoginActivity.this, new
+OnCompleteListener<AuthResult>() {
+ @Override
+public void onComplete(@NonNull Task<AuthResult> task) {
+ if(!task.isSuccessful()){
+ Toast.makeText(DriverLoginActivity.this, "sign up
+error", Toast.LENGTH_SHORT).show();
+ }
+  else{
+ String user_id = mAuth.getCurrentUser().getUid();
+ DatabaseReference current_user_db =
+FirebaseDatabase.getInstance().getReference().child("Users").child("Drivers").child(user_id);
+ current_user_db.setValue(true);
+               }
+           }
+       });
+     }
+ });
+ mLogin.setOnClickListener(new View.OnClickListener() {
+   @Override
+     public void onClick(View v) {
+     final String email = mEmail.getText().toString();
+     final String password = mPassword.getText().toString();
+     mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(RiderLoginActivity.this, new OnCompleteListener<AuthResult>() {
+  @Override
+  public void onComplete(@NonNull Task<AuthResult> task) {
+     if(!task.isSuccessful()){
+       Toast.makeText(DriverLoginActivity.this, "sign in error", Toast.LENGTH_SHORT).show();
+                   }
+               }
+           });
+         }
+     });
+}
+@Override
+protected void onStart() {
+ super.onStart();
+ mAuth.addAuthStateListener(firebaseAuthListener);
+}
+@Override
