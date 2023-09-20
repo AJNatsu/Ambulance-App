4d1d1680c3de372cac27b6a361afaba388fbdf1d
ABDULLAH BIN JASNI <107909543+AJNatsu@users.noreply.github.com>
2023-09-20 15:51:21 +0900

    Update Rider's Map.java

--- Rider's Map.java
+++ Rider's Map.java
@@ -1,11 +1,11 @@
 @Override
 protected void onCreate(Bundle savedInstanceState) {
-   super.onCreate(savedInstanceState);
-   setContentView(R.layout.activity_driver_map);
-   // Obtain the SupportMapFragment and get notified when the map is ready to be used.
- mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_driver_map);
 
- SupportMapFragment mapFragment = (SupportMapFragment)
-getSupportFragmentManager()
- .findFragmentById(R.id.map);
- mapFragment.getMapAsync(this); 
+    // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+    mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+    SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+    mapFragment.getMapAsync(this);
+}
+
