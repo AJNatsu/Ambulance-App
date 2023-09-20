51023cef3ff5f1d0742bfcd5af7b69e02358eebe
ABDULLAH BIN JASNI <107909543+AJNatsu@users.noreply.github.com>
2023-09-20 15:55:09 +0900

    Update main_layout.java

--- main_layout.java
+++ main_layout.java
@@ -1,22 +1,26 @@
 public class MainActivity extends AppCompatActivity {
-        
+    
     private Button mDriver, mRider;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         mDriver = (Button) findViewById(R.id.driver);
         mRider = (Button) findViewById(R.id.rider);
+
         mDriver.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Intent intent = new Intent(MainActivity.this,
-            DriverLoginActivity.class);
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DriverLoginActivity.class);
                 startActivity(intent);
                 finish();
-                return;
             }
-        }); 
+        });
+    }
+}
+
 <?xml version="1.0" encoding="utf-8"?>
 <LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
     xmlns:app="http://schemas.android.com/apk/res-auto"
@@ -24,4 +28,6 @@ public class MainActivity extends AppCompatActivity {
     android:layout_width="match_parent"
     android:layout_height="match_parent"
     tools:context=".MainActivity"
-    android:orientation="vertical"> 
+    android:orientation="vertical">
+
+</LinearLayout>
